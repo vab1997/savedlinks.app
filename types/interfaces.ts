436099787a928e3c8452187066fd1ea2f57c830a
Victor Bejas <victorbejas97@gmail.com>
2022-09-15 23:56:43 -0300

    add interface Link and Folder

--- types/interfaces.ts
+++ types/interfaces.ts
@@ -4,3 +4,18 @@ export interface User {
   name: string,
   email: string
 }
+
+export interface Link {
+  id: string,
+  link: string,
+  description: string,
+  read: boolean,
+  id_user: string,
+  id_folder: string
+}
+
+export interface Folder {
+  id: string,
+  id_user: string,
+  name: string
+}
